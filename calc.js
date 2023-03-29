9b213062dafc5572e637870ae2248c322763fd1b
Enrique Paulino <enrique-paulino@outlook.com>
2023-03-29 21:53:46 +0100

    Implemented clear button (all and partial clear)

--- calc.js
+++ calc.js
@@ -13,14 +13,19 @@ keys.addEventListener('click', e => {
 
         const previousKeyType = calculator.dataset.previousKeyType;
 
-        if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
+        if (!action) {        
+            if (
+                displayedNum === '0' || 
+                previousKeyType === 'operator' || 
+                previousKeyType === 'calculate'
+                ) {
                 display.textContent = keyContent
             }
             else {
                 display.textContent = displayedNum + keyContent;
             }
 
+            calculator.dataset.previousKeyType = 'number';
         }
 
         if (
@@ -29,26 +34,105 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
             ) {
-                key.classList.add('is-depressed');
-                calculator.dataset.firstValue = displayedNum;
-                calculator.dataset.previousKeyType = 'operator';
-                calculator.dataset.operator = action;
-            }
+                const firstValue = calculator.dataset.firstValue;
+                const operator = calculator.dataset.operator;
+                const secondValue = displayedNum;
+
+                if (
+                    firstValue && 
+                    operator && 
+                    previousKeyType !== 'operator' && 
+                    previousKeyType !== 'calculate'
+                    ) {
+
+                    const calcValue = calculate(firstValue, secondValue, operator);
+                    display.textContent = calcValue;
+
+                    calculator.dataset.firstValue = calcValue;
+                }
+                else {
+                    calculator.dataset.firstValue = displayedNum;
+                }
+
+                key.classList.add('is-depressed')
+                calculator.dataset.previousKeyType = 'operator'
+                calculator.dataset.operator = action
+        }
 
         if (action === 'decimal') {
-            display.textContent = displayedNum + '.'
+            if (!displayedNum.includes('.')) {            
+                display.textContent = displayedNum + '.'            
+            }
+            else if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = '0.';
+            }
+            calculator.dataset.previousKeyType = 'decimal';
+        }
+
+        if (action !== 'clear') {
+            const clearButton = calculator.querySelector('[data-action=clear]');
+            clearButton.textContent = 'CE';
         }
 
         if (action === 'clear') {
-            console.log('clear key pressed');
+            if (key.textContent === 'AC') {
+                calculator.dataset.firstValue = '';
+                calculator.dataset.operator = '';
+                calculator.dataset.secondValue = '';
+                calculator.dataset.modValue = '';
+            } else {
+                // CE Button Pressed
+
+                const displayedNum = display.textContent;
+                const lastChar = displayedNum.slice(-1);
+
+                display.textContent = displayedNum.slice(0, -1);
+
+                if (display.textContent === '') {
+                    display.textContent = '0';
+                }
+
+                if (lastChar === '.') {
+                    calculator.dataset.previousKeyType = 'decimal';
+                }
+
+                key.textContent = 'AC';
+            }
+
+            const clearButton = calculator.querySelector('[data-action=clear]');
+            if (
+                display.textContent === '0' ||
+                calculator.dataset.previousKeyType === 'operator'
+            ) {
+                clearButton.textContent = 'AC';
+            }
+            else {
+                clearButton.textContent = 'CE';
+            }
+        
+            
+            calculator.dataset.previousKeyType = 'clear';
         }
 
-        if (action === 'calculate') {
-            const firstValue = calculator.dataset.firstValue;
-            const secondValue = displayedNum;
-            const operator = calculator.dataset.operator;
+        
 
-            display.textContent = calculate(firstValue, secondValue, operator);
+        if (action === 'calculate') {
+            let firstValue = calculator.dataset.firstValue;            
+            let secondValue = displayedNum;
+            const operator = calculator.dataset.operator;
+            
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                  firstValue = displayedNum;
+                  secondValue = calculator.dataset.modValue;
+                }
+                
+                display.textContent = calculate(firstValue, secondValue, operator);
+            }
+            
+          // Set modValue attribute
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = 'calculate';
         }
 
     }
